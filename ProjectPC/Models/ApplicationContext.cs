efd7aa18a839d226338d988955692c2b31f8e458
Vicram RJ Purshotam <vicram.purshotam@gmail.com>
2017-09-02 01:14:36 +0200

    Created Product and Category Controllers

---
+++ ProjectPC/Models/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ProjectPC.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext() : base() { }
+
+        public DbSet<Product>           Products            { get; set; }
+        public DbSet<Category>          Categories          { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ ProjectPC/Models/Product.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectPC.Models
+{
+    public class Product
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int          ProductID           { get; set; }
+
+        [Required, Display(Name = "Name")]
+            public string       ProductName         { get; set; }
+
+        [Required, Display(Name = "Description"), DataType(DataType.MultilineText)]
+            public string       ProductDescription  { get; set; }
+
+        [Display(Name = "Size 1")]
+            public bool         Size1               { get; set; }
+
+        [Display(Name = "Size 2")]
+            public bool         Size2               { get; set; }
+
+        [Display(Name = "Size 3")]
+            public bool         Size3               { get; set; }
+
+        [Required, Display(Name = "Price"), DataType(DataType.Currency)]
+            public double       Price1              { get; set; }
+
+        [Display(Name = "Price"), DataType(DataType.Currency)]
+            public double       Price2              { get; set; }
+
+        [Display(Name = "Price"), DataType(DataType.Currency)]
+            public double       Price3              { get; set; }
+
+        [Display(Name = "Product Image")]
+            public byte[]       Image               { get; set; }
+
+        [Display(Name = "Image Path")]
+            public string       ImageType           { get; set; }
+        
+        [Display(Name = "Category")]
+            public int          CategoryID          { get; set; }
+
+        [ForeignKey("CategoryID")]
+            public Category     Category            { get; set; }
+
+    }
+}
